833225f4280116fafd684154996e536f5b21d3ec
GeorgeCTSGit <georgevelivil.joseph@cognizant.com>
2023-08-31 01:05:52 +0530

    server change

--- src/main/java/com/packagecentre/ms/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
+++ src/main/java/com/packagecentre/ms/netflixeurekanamingserver/NetflixEurekaNamingServerApplication.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class NetflixEurekaNamingServerApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(NetflixEurekaNamingServerApplication.class, args);
 	}
 
